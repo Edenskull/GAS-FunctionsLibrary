fde47b435b8346dee28e2f41e7a3370a390b62ee
EdenSkull <maxchamp@live.fr>
2021-10-09 01:22:46 +0200

    Reduce and optimize Array.js

--- Code/Array/Array.js
+++ Code/Array/Array.js
@@ -1,14 +1,16 @@
 /**
- * Function to change array items case (lower or upper). Work with Spreadsheet array of values (.getValues()). But it doesn't convert Dates, Numbers, Currencies...
+ * Function to change array (could be more than 2D) items case (lower or upper). Work with Spreadsheet array of values (.getValues()). But it doesn't convert Dates, Numbers, Currencies...
  * @param {array} array - Array that need case update
- * @param {bool} recurse - If the array is flat or need recursive method
  * @param {string} mode - You have choice between "upper", "lower", "title"
  * @returns {Array} New Array with updated case.
  */
-function changeArrayCase(array, recurse, mode) {
-    if (!recurse) {
-        return array.map((item) => {
-            if (item.constructor != Number || item.constructor != Date) {
+function changeArrayCase(array, mode) {
+    return array.map((item) => {
+        Logger.log(item.constructor);
+        if (item.constructor == Array) {
+            return changeArrayCase(item, mode);
+        } else {
+            if (item.constructor != Number && item.constructor != Date) {
                 if (mode == "lower") {
                     result = item.toLowerCase();
                 } else if (mode == "upper") {
@@ -20,41 +22,36 @@ function changeArrayCase(array, recurse, mode) {
             } else {
                 return item;
             }
-        });
-    } else {
-        return array.map((item) => {
-            if (item.constructor == Array) {
-                return changeArrayCase(item, recurse, mode);
-            } else {
-                if (item.constructor == Number || item.constructor == Date) {
-                    return item;
-                } else {
-                    if (mode == "lower") {
-                        result = item.toLowerCase();
-                    } else if (mode == "upper") {
-                        result = item.toUpperCase();
-                    } else if (mode == "title") {
-                        result = item.substring(0, 1).toUpperCase() + item.substring(1).toLowerCase();
-                    }
-                    return result;
-                }
-            }
-        });
-    }
+        }
+    });
 }
 
 /**
  * Function to remove duplicates in 2D arrays.
  * @param {array} array - Array that need to be pruned
+ * @param {bool} ignoreCase - If you want to ignore case
  * @returns {Array} New Array without duplicates.
  */
-function multiDimensionalUnique(arr) {
+function multiDimensionalUnique(array, ignoreCase) {
     var uniques = [];
     var itemsFound = {};
-    for (var i = 0, l = arr.length; i < l; i++) {
-        var stringified = JSON.stringify(arr[i]);
-        if (itemsFound[stringified]) { continue; }
-        uniques.push(arr[i]);
+    for (var i = 0, l = array.length; i < l; i++) {
+        var stringified;
+        if (ignoreCase) {
+            stringified = JSON.stringify(array[i].map((item) => {
+                if (item.constructor != Number && item.constructor != Date) {
+                    return item.toLowerCase();
+                } else {
+                    return item;
+                }
+            }));
+        } else {
+            stringified = JSON.stringify(array[i]);
+        }
+        if (itemsFound[stringified]) {
+            continue;
+        }
+        uniques.push(array[i]);
         itemsFound[stringified] = true;
     }
     return uniques;
